c80f3217b1986042e6dd34b557560a41b1441a03
Vladislav Dokuchaev <vodokuchaev@edu.hse.ru>
2026-07-08 23:25:05 +0300

    My try 3

---
+++ adp/ADP_main.py
@@ -0,0 +1,45 @@
+class Data:
+    def Generate_X():
+        pass
+
+    def Generate_Noise():
+        pass
+
+    def Generate_func():
+        pass
+
+    def Generate_beta():
+        pass
+
+
+class EDR:
+    def __init__(self) -> None:
+        self.Data = Data()
+
+    pass
+
+    def Mean_Calculate():
+        pass
+
+    def Generate_Direction():
+        pass
+
+    def Weight_Calculate():
+        pass
+
+    def Rho_Calculate():
+        pass
+
+    def Generate_Data():
+        pass
+
+
+class EDR_ADP_single_index(EDR):
+    def run_step_0():
+        pass
+
+    def run_step_k():
+        pass
+
+    def run():
+        pass
--- adp/variants/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from .random_projection import RandomProjectionADP
-
-__all__ = ["RandomProjectionADP"]
